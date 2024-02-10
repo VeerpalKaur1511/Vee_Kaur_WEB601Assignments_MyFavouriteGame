b010fa648b92f6536b5bad4b92b551a11a41b570
VeerpalKaur1511 <122822806+VeerpalKaur1511@users.noreply.github.com>
2024-02-09 19:59:10 -0500

    Assignment 3

--- MyFavouriteGames/src/app/content-list/content-list.component.ts
+++ MyFavouriteGames/src/app/content-list/content-list.component.ts
@@ -15,61 +15,111 @@ export class ContentListComponent {
     console.log("Content ID:", id);
     console.log("Content Title:", title);
   }
-  
+  typePresent:boolean | undefined;
+  inputValue:string='';
   
   constructor(){
   this.contentArray.push(
     
       { 
         id: 1, 
-        title: 'Badminton', description: 'Badminton, court or lawn game ', creator: 'The Duke of Beaufort', imgURL: 'https://www.games-eshop.com/wp-content/uploads/2020/09/Badminton_Den_Haag-scaled-2-2048x1365.jpg', type: 'Indoor',
+        title: 'Badminton',
+         description: 'Badminton, court or lawn game ',
+          creator: 'The Duke of Beaufort', 
+          imgURL: 'https://www.games-eshop.com/wp-content/uploads/2020/09/Badminton_Den_Haag-scaled-2-2048x1365.jpg', 
+          type: '',
         tags: ['Tag1', 'Tag2'] 
       },
       { 
         id: 2, 
-        title: 'Football', description: 'Association football or soccer', creator: 'James Haldane ', imgURL: 'https://mir-s3-cdn-cf.behance.net/project_modules/fs/f8c677104758261.5f6a25a1a5f8d.jpg', type: 'Outdoor',
+        title: 'Football',
+         description: 'Association football or soccer', 
+         creator: 'James Haldane ', 
+         imgURL: 'https://mir-s3-cdn-cf.behance.net/project_modules/fs/f8c677104758261.5f6a25a1a5f8d.jpg',
+          type: '',
         tags: ['Tag3', 'Tag6'] 
       },
       { 
         id: 3, 
-        title: 'Racing', description: 'Racing is my favourite', creator: 'Jakson Wild', imgURL: 'https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/e7783bd1-cbb5-42ac-87ce-053670c4b357_scaled.jpg', type: 'Outdoor',
+        title: 'Racing', 
+        description: 'Racing is my favourite', 
+        creator: 'Jakson Wild', 
+        imgURL: 'https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/e7783bd1-cbb5-42ac-87ce-053670c4b357_scaled.jpg', 
+        type: '',
         tags: ['Tag5', 'Tag6'] 
       },
       { 
         id: 4, 
-        title: 'No name for this', 
+        title: 'Cash ', 
         description: 'Description for all games', 
         creator: 'Harry tho', 
         imgURL: 'https://www.wallpaperup.com/uploads/wallpapers/2014/06/24/377589/38350a3152f132e5dbc4de5e802734b4.jpg', 
-        type: 'computer', 
+        type: 'Computer', 
         tags: ['Tag5', 'Tag6'] 
       },  { 
         id: 5, 
-        title: 'Cycling', 
+        title: 'Battleground', 
         description: 'Cycling is goods for health', 
         creator: 'Nature', 
-        imgURL: 'https://th.bing.com/th/id/OIP.TDlor4RPF_NhbTDUCN4qSAHaE8?rs=1&pid=ImgDetMain', 
-        type: 'outdoor', 
+        imgURL: 'https://th.bing.com/th/id/OIP.QiY9_ch3mhhluBEZvDlsFAHaEO?rs=1&pid=ImgDetMain', 
+        type: 'Computer', 
         tags: ['Tag5', 'Tag6'] 
       },
       { 
         id: 6, 
-         title: 'Long Jump ', description: 'Long jump is my favourite', creator: 'gurdeep', imgURL: 'https://assetstorev1-prd-cdn.unity3d.com/package-screenshot/e7783bd1-cbb5-42ac-87ce-053670c4b357_scaled.jpg', type: 'Outdoor',
+        title: 'Snipper', 
+        description: 'Cycling is goods for health', 
+        creator: 'Nature', 
+        imgURL: 'https://th.bing.com/th/id/OIP.x2QCGE8Vx50S1KP7raLTwAHaEK?w=312&h=180&c=7&r=0&o=5&dpr=1.5&pid=1.7', 
+        type: 'Computer', 
         tags: ['Tag5', 'Tag6'] 
-      }
+      },
+      { 
+        id: 7, 
+        title: 'Hockey', 
+        description: 'Cycling is goods for health', 
+        creator: 'Nature', 
+        imgURL: 'https://th.bing.com/th/id/R.4952f346cd0a2489bdf8ca97ac2647e0?rik=BMQLgC1M4LIkMw&pid=ImgRaw&r=0', 
+        type: 'outdoor', 
+        tags: ['Tag5', 'Tag6'] 
+      },
+      { 
+        id: 8, 
+        title: 'Baseball', 
+        description: 'Cycling is goods for health', 
+        creator: 'Nature', 
+        imgURL: 'https://th.bing.com/th/id/R.ffb8dfba085441e83f6e4db8872649c4?rik=kLSyeJ17Bc5TIQ&pid=ImgRaw&r=0', 
+        type: 'outdoor', 
+        tags: ['Tag5', 'Tag6'] 
+      },
+      { 
+        id: 9, 
+        title: 'Cycling', 
+        description: 'Cycling is goods for health', 
+        creator: 'Nature', 
+        imgURL: 'https://omgnepal.com/wp-content/uploads/2020/07/Best-Beginner-Road-Bikes-gear-patrol-full-lead.jpg', 
+        type: 'outdoor', 
+        tags: ['Tag5', 'Tag6'] 
+      },
+      
      );
-    
-
-   
-    
-
-
-
-
-    
-    
-    
-      // Add more content items if needed
+       // Add more content items if needed
     }
+
+
+search(str:string){
+     
+  for(let  i=0; i < this.contentArray.length; i++){
+    if(this.contentArray[i].title==str)
+    {
+      this.typePresent=true;
+      break;
+    }
+  
+  else if(this.contentArray[i].title!=str)
+   this.typePresent=false;
+  }
+      
+    }
 
 }
